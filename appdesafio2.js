c48711b3a9984cbe8b579f1f4a81e7a57651c776
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-04-22 21:50:25 -0300

    Update appdesafio2.js

--- appdesafio2.js
+++ appdesafio2.js
@@ -155,3 +155,28 @@ function tabuada(numero){
     return alert(`a tabuada de ${numero} é: ${numerox1}, ${numerox2}, ${numerox3}, ${numerox4}, ${numerox5}, ${numerox6}
         ${numerox7}, ${numerox8}, ${numerox9}, ${numerox10}`);
 }
+
+
+
+
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica)
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos:
+// 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java','Ruby','GoLang');
+console.log(linguagensDeProgramacao)
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaDeNomes = ['Felipe', 'Marco', 'Duarte'];
+console.log(listaDeNomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaDeNomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaDeNomes[2]);
