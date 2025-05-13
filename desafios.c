360cf1ccdcd0ece30943e1ed1e3ede58bcb1fe79
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-05-13 13:00:45 -0300

    Add files via upload

---
+++ desafios.c
@@ -0,0 +1,18 @@
+// Escreva um programa que peça para o usuário digitar duas variáveis inteiras (x e y) e
+// imprima o resultado da multiplicação entre essas duas variáveis.
+
+#include <stdio.h>
+
+int main() {
+    int varX;
+    int varY;
+
+    printf("digite a variavel x\n");
+    scanf("%d", &varX);
+    printf("digite a variavel y\n");
+    scanf("%d", &varY);
+
+    int resposta = varX * varY;
+    printf("o resultado da multiplicacao entre %d e %d: %d", varX, varY, resposta);
+}
+
