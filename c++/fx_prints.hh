97f87f2437eb5d0fe23971030b1d06229c95c6cf
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2026-03-03 09:31:20 -0300

    Convert mensagem_inicial to inline function

--- c++/fx_prints.hh
+++ c++/fx_prints.hh
@@ -5,5 +5,12 @@ using namespace std;
 
 void print_forca();
 void print_errados(int tent);
-void mensagem_inicial();
+
+inline void mensagem_inicial() { // inline: "compilador, se voce achar que é bom, copie essa função para onde ela está sendo usada no main" 
+    cout<<"******************************"<<endl;
+    cout<<"* bem vindo ao jogo da forca *"<<endl;
+    cout<<"******************************"<<endl;
+    print_forca();
+}
+
 void mensagem_final();
